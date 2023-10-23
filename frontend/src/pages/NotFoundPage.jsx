aafd1ae62490d82abbf22769cd0373819706b4a3
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-10-23 14:20:38 +0300

    notFound404

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound404 from '../images/notFound404.jpg';
 
 const NotFoundPage = () => {
   return (
-    <div>
-        <h1> 404 - Страница не найдена</h1>
+    <div className="text-center">
+      <img
+        src={notFound404}
+        width={250}
+        height={250}
+        alt="страница не найдена"
+        className="img-fluid h-20"
+      />
+      <h1 className="text-muted h4">страница не найдена</h1>
+      <p className="text-muted">
+        но вы можете
+        <Link to="/">На главную страницу</Link>
+      </p>
     </div>
   );
 };
