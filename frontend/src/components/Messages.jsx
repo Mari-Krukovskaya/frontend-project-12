6f42bc8ea04c9ed21fb0556946cd2debb9c6e8ca
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-11-22 19:06:48 +0300

    linter error

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -76,30 +76,30 @@ const Messages = () => {
       <div id="messages-box" className="chat-messages overflow-auto px-5" ref={msgRefInput}>
         {filteredMessages.map((message) => (
           message.username === user.username ? (
-                <div
-                  key={message.id}
-                  className="text-break mb-2"
-                >
-                  <b>{message.username}</b>
-                  :
-                  {' '}
-                  {message.body}
-                </div>
+            <div
+              key={message.id}
+              className="text-break mb-2"
+            >
+             <b>{message.username}</b>
+              :
+              {' '}
+              {message.body}
+            </div>
           ) : (
-                <div
-                  key={message.id}
-                  className="text-break mb-2"
-                  ref={msgRefInput}
-                >
-                  <div className="d-inline-flex bg-body p-2 rounded border">
-                    <div>
-                      <b>{message.username}</b>
-                    </div>
-                    :
-                    {' '}
-                    {message.body}
-                  </div>
+            <div
+              key={message.id}
+              className="text-break mb-2"
+              ref={msgRefInput}
+            >
+              <div className="d-inline-flex bg-body p-2 rounded border">
+                <div>
+                  <b>{message.username}</b>
                 </div>
+                :
+                {' '}
+                {message.body}
+              </div>
+            </div>
           )
         ))}
       </div>
@@ -110,40 +110,40 @@ const Messages = () => {
           className="py-1 border rounded-2"
         >
           <InputGroup className="input-group has-validation">
-                <Form.Control
-                  type="text"
-                  required
-                  className="border-0 p-0 ps-2"
-                  onChange={formik.handleChange}
-                  value={formik.values.body}
-                  name="body"
-                  aria-label={t('messages.newMessage')}
-                  ref={refInput}
-                  onBlur={formik.handleBlur}
-                  placeholder={t('messages.messagePlaceholder')}
-                  disabled={formik.isSubmitting}
+            <Form.Control
+              type="text"
+              required
+              className="border-0 p-0 ps-2"
+              onChange={formik.handleChange}
+              value={formik.values.body}
+              name="body"
+              aria-label={t('messages.newMessage')}
+              ref={refInput}
+              onBlur={formik.handleBlur}
+              placeholder={t('messages.messagePlaceholder')}
+              disabled={formik.isSubmitting}
+            />
+            <Button
+              type="submit"
+              variant="group-vertical"
+              disabled={isButtonDisable}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                width="20"
+                height="20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
                 />
-                <Button
-                  type="submit"
-                  variant="group-vertical"
-                  disabled={isButtonDisable}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
-                    />
-                  </svg>
-                  <span className="visually-hidden">
-                    {t('messages.enterMessage')}
-                  </span>
-                </Button>
+              </svg>
+              <span className="visually-hidden">
+                {t('messages.enterMessage')}
+              </span>
+            </Button>
           </InputGroup>
         </Form>
       </div>
